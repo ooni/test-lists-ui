421f6c7c94a02babaafa1a9db9cb3331956e4af2
Sarath M S <sarath.ms@gmail.com>
2022-06-01 16:56:46 -0400

    Redirect /login to home page if already logged in

--- pages/login.js
+++ pages/login.js
@@ -8,6 +8,7 @@ import LoginForm from '../components/LoginForm'
 import { apiEndpoints, loginUser } from '../components/lib/api'
 import { mutate } from 'swr'
 import Loading from '../components/Loading'
+import { useUser } from '../components/lib/hooks'
 import QuickStartGuide from '../components/submit/QuickStartGuide'
 
 const Login = () => {
@@ -16,6 +17,15 @@ const Login = () => {
   const [error, setError] = useState(null)
   const router = useRouter()
   const { token, returnTo = '/' } = router.query
+
+  const { user, loading } = useUser()
+
+  // If user is already logged in, redirect to home page
+  useEffect(() => {
+    if (!loading && user.loggedIn) {
+      router.replace('/')
+    }
+  }, [user, loading, router])
 
   const onLoginSubmit = useCallback(() => {
     // After submitting the login form
