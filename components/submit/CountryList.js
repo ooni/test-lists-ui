f23c35eb2e3a57c5cb832dc1fc1bb033065ef106
Maja Komel <maja.komel@gmail.com>
2022-07-27 17:18:19 +0200

    Fix value for global dropdown option

--- components/submit/CountryList.js
+++ components/submit/CountryList.js
@@ -6,7 +6,7 @@ const CountryList = ({ defaultValue, ...rest }) => {
   return (
     <Select {...rest} value={defaultValue}>
       <option value=''>Select a country</option>
-      <option value='global'>Global</option>
+      <option value='GLOBAL'>Global</option>
       {countryUtil.countryList
         .sort((c1, c2) => c1.iso3166_name > c2.iso3166_name ? 1 : -1)
         .map(({ iso3166_alpha2, name }, index) => (
